51d9a08b0bf0ce0fddfc6b85315e6ca83ad67d76
Alice Tomaz <tomaz.alice@gmail.com>
2025-05-24 14:51:56 -0300

    fix

--- terraform/modules/lambda/output.tf
+++ terraform/modules/lambda/output.tf
@@ -2,3 +2,8 @@ output "fastfood_auth_lambda_arn" {
   description = "The ARN of the Lambda function"
   value       = aws_lambda_function.fastfood_auth_lambda.arn
 }
+
+output "fastfood_auth_lambda_invoke_arn" {
+  description = "The Invoke ARN of the Lambda function"
+  value       = aws_lambda_function.fastfood_auth_lambda.invoke_arn
+}
