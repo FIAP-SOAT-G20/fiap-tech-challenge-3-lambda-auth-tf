ea1d168a1f261f11e961204dc4fdd42f5be2c5de
Alice Tomaz <tomaz.alice@gmail.com>
2025-05-24 15:10:40 -0300

    fix: stage

--- terraform/modules/apigateway-integrations/stage.tf
+++ terraform/modules/apigateway-integrations/stage.tf
@@ -1,11 +1,11 @@
-# resource "aws_api_gateway_stage" "auth_stage" {
-#   deployment_id = aws_api_gateway_deployment.auth_deployment.id
-#   rest_api_id   = var.api_gateway_id
-#   stage_name    = "prod"
-# }
+resource "aws_api_gateway_stage" "auth_stage" {
+  deployment_id = aws_api_gateway_deployment.auth_deployment.id
+  rest_api_id   = var.api_gateway_id
+  stage_name    = "prod"
+}
 
-# resource "aws_api_gateway_stage" "api_stage" {
-#   deployment_id = aws_api_gateway_deployment.api_deployment.id
-#   rest_api_id   = var.api_gateway_id
-#   stage_name    = "prod"
-# }
+resource "aws_api_gateway_stage" "api_stage" {
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  rest_api_id   = var.api_gateway_id
+  stage_name    = "prod"
+}
